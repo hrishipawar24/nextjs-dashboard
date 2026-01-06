da528fbd1581a75853ac5b0df0fc68ae25b67cb6
Hrishikesh Pawar <pawarhrishikesh24@gmail.com>
2026-01-06 13:22:10 +0530

    added Login

--- auth.config.ts
+++ auth.config.ts
@@ -7,14 +7,21 @@ export const authConfig = {
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
-      if (isOnDashboard) {
-        if (isLoggedIn) return true;
-        return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+      const isOnLogin = nextUrl.pathname.startsWith('/login');
+      const isOnRoot = nextUrl.pathname === '/';
+
+      if (isLoggedIn) {
+        if (isOnLogin || isOnRoot) {
+          return Response.redirect(new URL('/dashboard', nextUrl));
+        }
+        return true;
       }
-      return true;
+
+      if (isOnLogin) {
+        return true;
+      }
+
+      return false; // Redirect unauthenticated users to login page
     },
   },
   providers: [], // Add providers with an empty array for now
