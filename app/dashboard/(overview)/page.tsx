391c85b431cc9252857256097fda68618dbe3671
Hrishikesh Pawar <pawarhrishikesh24@gmail.com>
2026-01-06 13:03:48 +0530

    till chapter 15

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,3 +1,8 @@
+import { Metadata } from 'next';
+ 
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
 import CardWrapper from '@/app/ui/dashboard/cards';
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
